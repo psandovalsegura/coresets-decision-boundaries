d0f0d7915a15e1683bfc935b35218c55e0c6a080
Pedro Sandoval <psandovalsegura@gmail.com>
2021-10-27 20:15:16 -0400

    add random splits experiment

--- main.py
+++ main.py
@@ -10,18 +10,32 @@ import torchvision.transforms as transforms
 
 import os
 import argparse
+import numpy as np
 
 from models import *
-from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
+parser.add_argument('--cifar_dir', default='./data',
+                    help='location of the cifar-10 dataset')
+parser.add_argument('--epochs', default=100, type=int, help='number of epochs')
+parser.add_argument('--workers', default=1, type=int, help='number of data loading workers')
+parser.add_argument('--batch_size', default=128, type=int, help='input batch size')
+parser.add_argument('--runs', default=1, type=int, help='number of runs')
+parser.add_argument('--random_split', action='store_true',
+                    help='whether to train on a random split of the training set')
+parser.add_argument('--split_fraction', type=float, default=0.5,
+                    help='the fraction of samples for the random split')
+parser.add_argument('--no_checkpoint_save', action='store_true', help='whether to save checkpoint')
+parser.add_argument('--no_progress_bar', action='store_true', help='whether to show progress bar')
+parser.add_argument('--no_download_data', action='store_true', help='whether to download data')
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'==> Using device {device}..')
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
@@ -40,14 +54,21 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+    root=args.cifar_dir, train=True, download=(not args.no_download_data), transform=transform_train)
+if args.random_split:
+    split_size = int(args.split_fraction * len(trainset))
+    train_idx = torch.randperm(len(trainset))[:split_size]
+    train_idx = train_idx.tolist()
+    train_set = torch.utils.data.Subset(trainset, train_idx)
+    print(f'==> Using a random training set split of size {len(train_set)}..')
+
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
 
 testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+    root=args.cifar_dir, train=False, download=(not args.no_download_data), transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
@@ -55,7 +76,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 # Model
 print('==> Building model..')
 # net = VGG('VGG19')
-# net = ResNet18()
+net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -68,7 +89,7 @@ print('==> Building model..')
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-net = SimpleDLA()
+# net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -91,7 +112,6 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 # Training
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
@@ -108,9 +128,11 @@ def train(epoch):
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
-
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        
+        if not args.no_progress_bar:
+            from utils import progress_bar
+            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                        % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 
 def test(epoch):
@@ -130,13 +152,16 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if not args.no_progress_bar:
+                from utils import progress_bar
+                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
-    if acc > best_acc:
-        print('Saving..')
+    print(f'[Epoch {epoch}] Test Accuracy: {acc:.2f}%')
+    if acc > best_acc and not args.no_checkpoint_save:
+        print('Saving checkpoint..')
         state = {
             'net': net.state_dict(),
             'acc': acc,
@@ -146,9 +171,16 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
+    return acc
 
+accs = []
+for run in range(args.runs):
+    print(f'[Run {run}]')
+    for epoch in range(start_epoch, args.epochs):
+        train(epoch)
+        acc = test(epoch)
+        scheduler.step()
+    accs.append(acc)
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+print(f'Mean accuracy: {np.mean(np.array(accs))}, \
+        Std_error: {np.std(np.array(accs))/np.sqrt(args.runs)}')
---
+++ scripts/train_random_splits.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=rand-splits
+#SBATCH --time=1-12:00:00
+#SBATCH --partition=dpart
+#SBATCH --qos=high
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:p6000:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=end          
+#SBATCH --mail-type=fail         
+#SBATCH --mail-user=psando@umd.edu
+# -- SBATCH --dependency=afterok:
+
+set -x
+
+export WORK_DIR="/scratch0/slurm_${SLURM_JOBID}"
+export SCRIPT_DIR="/cfarhomes/psando/Documents/coresets-decision-boundaries"
+
+# Set environment for attack
+mkdir $WORK_DIR
+python3 -m venv ${WORK_DIR}/tmp-env
+source ${WORK_DIR}/tmp-env/bin/activate
+pip3 install --upgrade pip
+pip3 install -r ${SCRIPT_DIR}/requirements.txt
+
+for frac in {0.1,0.3,0.5,0.7,0.9}
+do
+    python main.py --runs 5 --split_fraction $frac --workers 4 --batch_size 512 --random_split --no_progress_bar --no_checkpoint_save --no_download_data --cifar_dir /vulcanscratch/psando/cifar-10/
+done
